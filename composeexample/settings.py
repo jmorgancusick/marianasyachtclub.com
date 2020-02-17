ef571ac8eea32ac63bd24555c948cc82f8523d8d
John Cusick <losthuntet27@gmail.com>
2020-02-17 16:02:04 +1000

    gitignore and tmp postgresql password

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'example',
         'HOST': 'db',
         'PORT': 5432,
     }
